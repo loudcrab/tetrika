c5556f3a032568471a7971f23baa5660ba09d132
awdx9 <awdx94@mail.ru>
2019-10-03 07:21:13 +0300

    some changes

--- js/Task_3/Task_3.js
+++ js/Task_3/Task_3.js
@@ -1,9 +1,7 @@
 let elem2 = document.getElementById("id2");
 let elem3 = document.getElementById("id3");
 
-elem2.addEventListener("click", (e) => console.log(e.currentTarget.style.background)
-)
-;
+elem2.addEventListener("click", (e) => console.log(e.currentTarget.style.background))
 elem3.addEventListener("click", (e) => {
     //Запрещаем всплытие
         e.stopPropagation();
